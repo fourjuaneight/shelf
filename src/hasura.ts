a802dfcd916e10e3c294652a45c0a4b18dcfece2
Juan Villela <juan@villela.co>
2022-05-31 16:07:24 -0400

    feat: Check for existing shelf item before adding new.

--- src/hasura.ts
+++ src/hasura.ts
@@ -7,6 +7,112 @@ import {
 } from './typings.d';
 
 /**
+ * Get shelf entries from Hasura.
+ * @function
+ * @async
+ *
+ * @returns {Promise<ShelfItem[]>}
+ */
+export const queryShelfItems = async (): Promise<ShelfItem[]> => {
+  const query = `
+    {
+      media_shelf(order_by: {name: asc}) {
+        category
+        comments
+        completed
+        cover
+        creator
+        genre
+        name
+        rating
+      }
+    }
+  `;
+
+  try {
+    const request = await fetch(`${HASURA_ENDPOINT}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Hasura-Admin-Secret': `${HASURA_ADMIN_SECRET}`,
+      },
+      body: JSON.stringify({ query }),
+    });
+    const response: HasuraQueryResp | HasuraErrors = await request.json();
+
+    if (response.errors) {
+      const { errors } = response as HasuraErrors;
+
+      console.log(errors);
+      throw `Querying records from Hasura - Shelf: \n ${errors
+        .map(err => `${err.extensions.path}: ${err.message}`)
+        .join('\n')} \n ${query}`;
+    }
+
+    return (response as HasuraQueryResp).data.media_shelf;
+  } catch (error) {
+    console.log(error);
+    throw `Querying records from Hasura - Shelf: \n ${error}`;
+  }
+};
+
+/**
+ * Search shelf entries from Hasura.
+ * @function
+ * @async
+ *
+ * @param {string} term shelf item name
+ * @returns {Promise<ShelfItem[]>}
+ */
+export const searchShelfItems = async (
+  pattern: string
+): Promise<ShelfItem[]> => {
+  const query = `
+    {
+      media_shelf(
+        order_by: {name: asc},
+        where: {name: {_iregex: ".*${pattern}.*"}}
+      ) {
+        category
+        comments
+        completed
+        cover
+        creator
+        genre
+        name
+        rating
+      }
+    }
+  `;
+
+  try {
+    const request = await fetch(`${HASURA_ENDPOINT}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Hasura-Admin-Secret': `${HASURA_ADMIN_SECRET}`,
+      },
+      body: JSON.stringify({ query }),
+    });
+    const response: HasuraQueryResp | HasuraErrors = await request.json();
+
+    if (response.errors) {
+      const { errors } = response as HasuraErrors;
+
+      console.log(errors);
+      throw `Searching records from Hasura - Shelf: \n ${errors
+        .map(err => `${err.extensions.path}: ${err.message}`)
+        .join('\n')} \n ${query}`;
+    }
+
+    return (response as HasuraQueryResp).data.media_shelf;
+  } catch (error) {
+    console.log(error);
+    throw `Searching records from Hasura - Shelf: \n ${error}`;
+  }
+};
+
+/**
  * Add shelf entry to Hasura.
  * @function
  * @async
@@ -33,6 +139,14 @@ export const addShelfItem = async (item: ShelfItem): Promise<string> => {
   `;
 
   try {
+    const existing = await searchShelfItems(item.name);
+
+    if (existing.length !== 0) {
+      console.log('addShelfItem', 'Shelf item already exists.');
+
+      throw `Adding record to Hasura: Shelf item already exists.`;
+    }
+
     const request = await fetch(`${HASURA_ENDPOINT}`, {
       method: 'POST',
       headers: {
@@ -118,111 +232,5 @@ export const updateShelfItem = async (
   } catch (error) {
     console.log(error);
     throw `Updating record to Hasura - Shelf: \n ${error}`;
-  }
-};
-
-/**
- * Get shelf entries from Hasura.
- * @function
- * @async
- *
- * @returns {Promise<ShelfItem[]>}
- */
-export const queryShelfItems = async (): Promise<ShelfItem[]> => {
-  const query = `
-    {
-      media_shelf(order_by: {name: asc}) {
-        category
-        comments
-        completed
-        cover
-        creator
-        genre
-        name
-        rating
-      }
-    }
-  `;
-
-  try {
-    const request = await fetch(`${HASURA_ENDPOINT}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Hasura-Admin-Secret': `${HASURA_ADMIN_SECRET}`,
-      },
-      body: JSON.stringify({ query }),
-    });
-    const response: HasuraQueryResp | HasuraErrors = await request.json();
-
-    if (response.errors) {
-      const { errors } = response as HasuraErrors;
-
-      console.log(errors);
-      throw `Querying records from Hasura - Shelf: \n ${errors
-        .map(err => `${err.extensions.path}: ${err.message}`)
-        .join('\n')} \n ${query}`;
-    }
-
-    return (response as HasuraQueryResp).data.media_shelf;
-  } catch (error) {
-    console.log(error);
-    throw `Querying records from Hasura - Shelf: \n ${error}`;
-  }
-};
-
-/**
- * Search shelf entries from Hasura.
- * @function
- * @async
- *
- * @param {string} term shelf item name
- * @returns {Promise<ShelfItem[]>}
- */
-export const searchShelfItems = async (
-  patter: string
-): Promise<ShelfItem[]> => {
-  const query = `
-    {
-      media_shelf(
-        order_by: {name: asc},
-        where: {name: {_iregex: ".*${patter}.*"}}
-      ) {
-        category
-        comments
-        completed
-        cover
-        creator
-        genre
-        name
-        rating
-      }
-    }
-  `;
-
-  try {
-    const request = await fetch(`${HASURA_ENDPOINT}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Hasura-Admin-Secret': `${HASURA_ADMIN_SECRET}`,
-      },
-      body: JSON.stringify({ query }),
-    });
-    const response: HasuraQueryResp | HasuraErrors = await request.json();
-
-    if (response.errors) {
-      const { errors } = response as HasuraErrors;
-
-      console.log(errors);
-      throw `Searching records from Hasura - Shelf: \n ${errors
-        .map(err => `${err.extensions.path}: ${err.message}`)
-        .join('\n')} \n ${query}`;
-    }
-
-    return (response as HasuraQueryResp).data.media_shelf;
-  } catch (error) {
-    console.log(error);
-    throw `Searching records from Hasura - Shelf: \n ${error}`;
   }
 };
